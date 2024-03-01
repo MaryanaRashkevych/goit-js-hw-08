569bd7b5b4e2a0cb43ee655d8239d340567baf77
MaryanaRashkevych <149114863+MaryanaRashkevych@users.noreply.github.com>
2024-03-01 02:52:00 +0200

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,44 @@
+// Напиши скрипт для створення галереї зображень на основі масиву даних. HTML містить список ul.gallery.
+
+// <ul class="gallery"></ul>
+
+// Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>.
+
+// Ти можеш створити й додати HTML-елементи, використовуючи document.createElement() і elem.append() або шаблонні рядки і elem.insertAdjacentHTML().
+
+// Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+// Додай мінімальне оформлення галереї флексбоксами через CSS класи.
+
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+const gallery = document.querySelector('.gallery');
+
+const imgList = images.map(img => {
+  const imgElem = document.createElement('img');
+  imgElem.classList.add('images');
+  imgElem.src = img.url;
+  imgElem.alt = img.alt;
+
+  const liElem = document.createElement('li');
+  liElem.classList.add('list');
+
+  liElem.append(imgElem);
+  return liElem;
+});
+
+console.log(imgList);
+gallery.append(...imgList);
+
+console.log(gallery);
---
+++ js/task-6.js
@@ -0,0 +1,69 @@
+// Напиши скрипт створення й очищення колекції елементів з наступним функціоналом.
+
+// Є input, у який користувач вводить бажану кількість елементів. Після натискання на кнопку Create має рендеритися (додаватися в DOM) колекція з відповідною кількістю елементів і очищатися значення в інпуті. При повторному натисканні на кнопку Create поверх старої колекції має рендеритись нова. Після натискання на кнопку Destroy колекція елементів має очищатися.
+
+// <div id="controls">
+//   <input type="number" min="1" max="100" step="1" />
+//   <button type="button" data-create>Create</button>
+//   <button type="button" data-destroy>Destroy</button>
+// </div>
+
+// <div id="boxes"></div>
+
+// Після натискання користувачем на кнопку Create треба провалідувати значення в input, воно має бути в межах від 1 до 100 включно. Тільки якщо воно задоволяє умову, мають додаватися нові <div> елементи в DOM.
+
+// Для рендеру елементів на сторінці створи функцію createBoxes(amount), яка приймає один параметр — число, що зберігає кількість елементів для рендеру.
+
+// Функція має створювати стільки <div> елементів, скільки вказано в параметрі amount і додавати їх у DOM дочірніми елементами для div#boxes.
+
+// Розміри першого <div> елемента мають бути 30px на 30px.
+// Кожен наступний елемент повинен бути ширшим і вищим від попереднього на 10px.
+// Усі елементи повинні мати випадковий колір фону. Використовуй готову функцію getRandomHexColor() для отримання випадкового кольору.
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+// Для очищення колекції після натискання на кнопку Destroy створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
+
+const input = document.querySelector('input');
+const controls = document.getElementById('controls');
+const createBtn = controls.querySelector('[data-create]');
+const destroyBtn = controls.querySelector('[data-destroy]');
+const boxesContainer = document.getElementById('boxes');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+createBtn.addEventListener('click', handleCreate);
+
+function handleCreate(event) {
+  const amount = input.valueAsNumber;
+  if (amount < 1 || amount > 100) {
+    alert('Please enter a number between 1 and 100');
+    return;
+  }
+
+  const boxes = [];
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+  }
+
+  boxesContainer.innerHTML = '';
+  boxesContainer.append(...boxes);
+}
+
+destroyBtn.addEventListener('click', handleDestroy);
+
+function handleDestroy() {
+  boxesContainer.innerHTML = '';
+}
